55dd814d00b5e56d927936047f6bb4dada3697a2
gashok13193 <35588473+gashok13193@users.noreply.github.com>
2023-08-03 21:04:16 +0530

    Create maven.groovy

---
+++ vars/maven.groovy
@@ -0,0 +1,8 @@
+def call(){
+  sh "echo maven clean is hapenning......."
+  sh "sleep 3"
+  sh "*****************IN PROGRESS***********"
+  sh "sleep 3"
+  sh "################## ALMOST THERE #########"
+  sh "mvn clean"
+}
